a457396ae83831b7caf061edee7b9dee4f61f630
PernilleRenkwitz <s204637@dtu.dk>
2025-10-28 10:31:54 +0100

    add: mRNA_to_codon function and documentation

---
+++ R/mRNA_to_codon.R
@@ -0,0 +1,18 @@
+#' mRNA_to_codon
+#'
+#' @param mRNA mRNA to transform
+#' @param start When to start the transformation. Default set to 1
+#'
+#' @returns Replaces mRNA with codon in string
+#' @export
+#'
+#' @examples
+#' # Usage
+#' mRNA_to_codon(mRNA, start)
+mRNA_to_codon <- function(mRNA, start = 1){
+  len_mRNA <- nchar(mRNA)
+  codons <- substring(mRNA,
+                      first = seq(from = start, to = len_mRNA-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = len_mRNA, by = 3))
+  return(codons)
+}
